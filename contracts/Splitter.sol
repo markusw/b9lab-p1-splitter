8e52ba42883ecda5c5c10c322fb33992c22d8a1d
Markus <syq3@pm.me>
2019-07-09 10:00:23 +0200

    Add Splitter contract

---
+++ contracts/Splitter.sol
@@ -0,0 +1,34 @@
+pragma solidity ^0.5.0;
+
+import "./Stoppable.sol";
+
+contract Splitter is Stoppable {
+
+    address payable public alice;
+    address payable public bob;
+    address payable public carol;
+
+    event LogFundsSplit(address split1, address split2, uint amount);
+
+    constructor(address payable _bob, address payable _carol) public {
+        alice = msg.sender;
+        bob = _bob;
+        carol = _carol;
+    }
+
+    function splitFunds() public payable onlyIfRunning returns(bool success) {
+        require(bob != address(0x0) && carol != address(0x0), "Avoid losing Ether");
+        require(msg.value > 0, "Needs ether");
+
+        if (msg.value % 2 != 0) {
+            alice.transfer(1);  // refund 1 wei if split amount is odd
+        }
+
+        bob.transfer(msg.value / 2);
+        carol.transfer(address(this).balance);
+
+        emit LogFundsSplit(bob, carol, msg.value);
+
+        return true;
+    }
+}
