5c1d4d9777b4b43e978ec101c93bcafab6bb3e9c
Markus <syq3@pm.me>
2019-07-16 12:00:12 +0200

    Rework web UI

--- app/js/app.js
+++ app/js/app.js
@@ -2,62 +2,77 @@ require("file-loader?name=../index.html!../index.html");
 const Web3 = require("web3");
 const Promise = require("bluebird");
 const truffleContract = require("truffle-contract");
+const assert = require("assert");
 const $ = require("jquery");
 
 const splitterJson = require("../../build/contracts/Splitter.json");
 
-if (typeof web3 !== 'undefined') {
-    window.web3 = new Web3(web3.currentProvider);
-} else {
-    window.web3 = new Web3(new Web3.providers.HttpProvider('http://localhost:7545')); 
-}
-
-const Splitter = truffleContract(splitterJson);
-Splitter.setProvider(web3.currentProvider);
-
 async function splitFunds() {
     try {
+        $("#error").html('');
+
         const instance = await Splitter.deployed();
-        accounts = await web3.eth.getAccounts();
+        const accounts = await web3.eth.getAccounts();
         const alice = accounts[1];
         const bob = accounts[2];
         const carol = accounts[3];
         const value = web3.utils.toWei($('input[name="amount"]').val(), "Ether")
 
-        const tx = await instance.splitFunds(bob, carol, {from: alice, value: value})
+        // trial run with call to see if transaction will be successful
+        assert(await instance.splitFunds.call(bob, carol, {from: alice, value: value}), 
+        "Transaction will fail, didn't send");
 
+        // Split funds
+        const tx = await instance.splitFunds(bob, carol, {from: alice, value: value}).on(
+            "transactionHash", 
+            txHash => $("#status1").html('Transaction sent, waiting to confirm. txHash: ' + txHash));
+        
         const receipt = tx.receipt;
+
         if (!receipt.status) {
             console.error(receipt);
-            $('#statusMessage').html('Error in transaction');
+            $("#error").html('Error in transaction: ' + receipt);
+        } else if (receipt.logs[0].event != "LogSplitFunds") {
+            console.error("Wrong event: " + receipt)
+            $("#error").html('Wrong event: ' + receipt);
         } else {
             console.log(receipt);
-            $('#statusMessage').html('Transaction successful, txHash: ' + receipt.transactionHash);
+            $("#status2").html('Transaction successful');
         }
     } catch (err) {
         console.error(err);
+        $("#error").html('Splitting funds failed: ' + err.toString());
     }
 };
 
 window.addEventListener('load', async() => {
+    if (typeof web3 !== 'undefined') {
+        window.web3 = new Web3(web3.currentProvider);
+    } else {
+        window.web3 = new Web3(new Web3.providers.HttpProvider('http://localhost:7545')); 
+    }
+    
+    Splitter = truffleContract(splitterJson);
+    Splitter.setProvider(web3.currentProvider);
+
     try {
-        accounts = await web3.eth.getAccounts();
+        const accounts = await web3.eth.getAccounts();
         if (accounts.length == 0) {
             $("#balance").html("N/A");
             throw new Error("No account to transact");
         }
-        accountAlice = accounts[1];
-        accountBob = accounts[2];
-        accountCarol = accounts[3];
+        const accountAlice = accounts[1];
+        const accountBob = accounts[2];
+        const accountCarol = accounts[3];
 
-        instance = await Splitter.deployed();
+        const instance = await Splitter.deployed();
         // get contract balances
-        balanceAlice = web3.utils.fromWei(await instance.balances.call(accountAlice), "Ether");
-        balanceBob = web3.utils.fromWei(await instance.balances.call(accountBob), "Ether");
-        balanceCarol = web3.utils.fromWei(await instance.balances.call(accountCarol), "Ether");
+        const balanceAlice = web3.utils.fromWei(await instance.balances.call(accountAlice), "Ether");
+        const balanceBob = web3.utils.fromWei(await instance.balances.call(accountBob), "Ether");
+        const balanceCarol = web3.utils.fromWei(await instance.balances.call(accountCarol), "Ether");
 
         // get ETH Balance of Alice
-        ethAlice = web3.utils.fromWei(await web3.eth.getBalance(accountAlice), "Ether");
+        const ethAlice = web3.utils.fromWei(await web3.eth.getBalance(accountAlice), "Ether");
 
         $("#balanceAlice").html(balanceAlice.toString(10));
         $("#balanceBob").html(balanceBob.toString(10));
@@ -68,5 +83,6 @@ window.addEventListener('load', async() => {
 
     } catch (err) {
         console.log(err);
+        $("#error").html(err.toString());
     }
 });
